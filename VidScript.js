50f72f02fa6223278d58a36ae11619b7aa501274
XfinityTools <138805049+XfinityTools@users.noreply.github.com>
2023-08-08 12:58:37 -0500

    Update VidScript.js

--- VidScript.js
+++ VidScript.js
@@ -1,1 +1,13 @@
+// Select the ad links that you want to prevent from opening in new tabs
+const adLinks = document.querySelectorAll('a.ad-link');
+
+// Function to prevent the default behavior of the ad links
+function blockAdLinks(event) {
+  event.preventDefault();
+}
+
+// Attach the event listener to block the ad links
+for (const adLink of adLinks) {
+  adLink.addEventListener('click', blockAdLinks);
+}
 
