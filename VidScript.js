67d52f56643575f41f57a8bb4e733c93bddaec5d
XfinityTools <138805049+XfinityTools@users.noreply.github.com>
2023-08-13 21:22:00 -0500

    Update VidScript.js

--- VidScript.js
+++ VidScript.js
@@ -49,19 +49,3 @@ function blockURLs(urlList) {
   blockURLs(blockedURLs);
   
 
-
-  document.addEventListener('click', function(event) {
-    const target = event.target;
-    
-    if (target.tagName === 'A') {
-      const href = target.getAttribute('href');
-      
-      // Check if the link is external (starts with http:// or https://)
-      if (href && /^https?:\/\//i.test(href)) {
-        event.preventDefault();
-        console.log('External link blocked:', href);
-        // You can add a message or other behavior here
-      }
-    }
-  });
-  
